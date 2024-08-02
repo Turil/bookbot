f669f5ee1e486cdd5784b6be2a84ac615d967369
Turil <turil@outlook.com>
2024-08-02 08:00:36 +0200

    add wordcount

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 def main():
-    with open(file="books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_text(book_path)
+    num_words = count_words(text)
+    print(f"{num_words} words found in the document")
 
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+def get_text(path):
+    with open(path) as f:
+        return f.read()
 
 if __name__ == '__main__':
     main()
