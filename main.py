8d2270124167b2772e9c869ca70532694ded0387
Turil <turil@outlook.com>
2025-01-03 14:12:18 +0100

    add count_characters function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
     text = get_text(book_path)
     num_words = count_words(text)
     print(f"{num_words} words found in the document")
+    print(count_characters(text))
 
 def count_words(text):
     words = text.split()
@@ -11,6 +12,18 @@ def count_words(text):
 def get_text(path):
     with open(path) as f:
         return f.read()
+
+
+def count_characters(text):
+    lowercase_text = text.lower()
+    my_dict = {}
+    for char in lowercase_text:
+        if char in my_dict:
+            my_dict[char] += 1
+        else:
+            my_dict[char] = 1
+    return my_dict
+
 
 if __name__ == '__main__':
     main()
